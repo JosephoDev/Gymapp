703ae6517575bb0d017f74769cdc55770196fd89
JosephoDev <josephmontini25@gmail.com>
2025-04-02 23:11:17 -0500

    ci

--- PROYECTO/reg.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect("localhost","root","","mydb") or die("<h2>Error no se cuentra la base de datos</h2> " . mysqli_error($link));
-$db = mysqli_select_db($link, "mydb") or die("Error " . mysqli_error($link));
-
-
-
-?>
