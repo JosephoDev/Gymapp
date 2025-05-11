ca47b74217564b6f0c7aa82e498d2c54e46a51e7
Andblanc19 <andres.ariasbba@cun.edu.co>
2025-05-10 19:53:47 -0500

    PRUEBAS

--- PROYECTO/main.py
+++ PROYECTO/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from models.connectdb import get_connection
-import pymysql
+import pymysql # type: ignore
 
 app = FastAPI()
 
